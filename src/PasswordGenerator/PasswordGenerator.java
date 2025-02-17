b5b71a02cd2a9aa2cf9a393da684751133565ab7
marcel-dittebrand-srhk <marcel.dittebrand.srhk@gmail.com>
2025-02-17 14:29:18 +0100

    Modified PasswordGenerator.java

--- src/PasswordGenerator/PasswordGenerator.java
+++ src/PasswordGenerator/PasswordGenerator.java
@@ -1,5 +1,6 @@
 package PasswordGenerator;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class PasswordGenerator {
@@ -8,14 +9,143 @@ public class PasswordGenerator {
 
 	public static void main(String[] args) {
 		
-		getPasswordStrength("test");
-
+		Scanner scanner = new Scanner(System.in);
+		
+		int passwordLength = 0;
+		
+		String userInput;
+		boolean isUserInputANumber;
+		boolean userWantsToExit = false;
+		
+		while(true) {
+			
+			System.out.printf(
+					"Program to generate a password and evaluate its strength%n" +
+					"%n"
+			);
+			
+			do {
+				
+				System.out.printf("Passwort-Generator%n");
+				System.out.printf("Length of password: ");
+				
+				isUserInputANumber = true;
+				
+				userInput = scanner.nextLine();
+				
+				for(int i = 0; i < userInput.length(); i++) {
+					
+					if(!(isANumber(userInput.charAt(i)))) {
+						
+						isUserInputANumber = false;
+						System.out.printf("Wrong input! User input should be numerical!%n%n");
+						break;
+						
+					}
+					
+				}
+				
+				if(isUserInputANumber) {
+					
+					passwordLength = Integer.parseInt(userInput);
+					
+				}
+				
+				if ((passwordLength < 6) && isUserInputANumber) {
+					
+					System.out.printf("Wrong input! Password length must be at least 6.%n%n");
+				}
+				
+			} while(!(isUserInputANumber) || passwordLength < 6);
+			
+			System.out.printf("%n%n");
+			
+			String generatedPassword = generatePassword(passwordLength);
+			
+			System.out.printf("%n");
+			
+			System.out.printf("Generated password: %s%n" , generatedPassword);
+			getPasswordStrength(generatedPassword);
+			
+			System.out.printf("%n%n");
+			
+			generatedPassword = generatePassword(passwordLength, true);
+			
+			System.out.printf("%n");
+			
+			System.out.printf("Generated password: %s%n" , generatedPassword);
+			getPasswordStrength(generatedPassword);
+			
+			do {
+				
+				System.out.printf("Do you want to exit the program (yes/no): ");
+				userInput = scanner.nextLine();
+				
+			} while ((userInput != "yes") || (userInput != "false"));
+			
+			if(userInput == "yes") {
+				
+				break;
+				
+			}
+		}
+		
+		
+		scanner.close();
+	}
+	
+	public static String generatePassword(int length) {
+		
+		String returnValue = "";
+		
+		Random random = new Random();
+		
+		for(int i = 0; i < length; i++) {
+			
+			returnValue += (char) (32 + random.nextInt(94));
+			
+		}
+		
+		return returnValue;
+	}
+	
+public static String generatePassword(int length, boolean justCharacters) {
+		
+		String returnValue = "";
+		
+		Random random = new Random();
+		
+		for(int i = 0; i < length; i++) {
+			
+			if(justCharacters) {
+				
+				if(random.nextBoolean()) {
+					
+					returnValue += (char) (65 + random.nextInt(25));
+					
+				} else {
+					
+					returnValue += (char) (97 + random.nextInt(25));
+					
+				}
+				
+			} else {
+				
+				returnValue += (char) (32 + random.nextInt(94));
+				
+			}
+			
+		}
+		
+		return returnValue;
 	}
 	
 	public static int getPasswordStrength(String password) {
 		
 		Scanner scanner = new Scanner(System.in);
+		
 		String output, tableLine;
+		final int tableSize = 40;
 		
 		int passwordStrength;
 		
@@ -25,8 +155,8 @@ public class PasswordGenerator {
 		boolean hasANumber = false;
 		boolean hasLessThan6Characters = password.length() < 6;
 		
-		output = getOutput(30, "Password strength of", password);
-		tableLine = getTableLine(30, output);
+		output = getOutput(tableSize, "Password strength of", password);
+		tableLine = getTableLine(tableSize, output);
 		
 		printTableLine(tableLine);
 		printOutput(output);
@@ -40,9 +170,24 @@ public class PasswordGenerator {
 			hasAUppercaseCharacter = isUppercase(currentCharacter) ? true : hasAUppercaseCharacter;
 			hasALowerCaseCharacter =  isLowercase(currentCharacter) ? true : hasALowerCaseCharacter;
 			hasANumber = isANumber(currentCharacter) ? true : hasANumber;
+			hasASpecialCharacter = 
+					!(
+							isUppercase(currentCharacter) ||
+							isLowercase(currentCharacter) ||
+							isANumber(currentCharacter) ||
+							currentCharacter == 32
+					) ? true: hasASpecialCharacter;
 			
 		}
 		
+		printOutput(getOutput(tableSize, "Has uppercase characters", hasAUppercaseCharacter ? "yes" : "no"));
+		printOutput(getOutput(tableSize, "Has lowercase characters", hasALowerCaseCharacter? "yes" : "no"));
+		printOutput(getOutput(tableSize, "Has numbers", hasANumber ? "yes" : "no"));
+		printOutput(getOutput(tableSize, "Has special characters", hasASpecialCharacter ? "yes" : "no"));
+		printOutput(getOutput(tableSize, "Has less than 6 characters", hasLessThan6Characters ? "yes" : "no"));
+		printTableLine(tableLine);
+		
+		scanner.close();
 		
 		return 0;
 	}
@@ -81,7 +226,7 @@ public class PasswordGenerator {
 	
 	public static void printOutput(String output) {
 		
-		System.out.printf(output + "%n");
+		System.out.printf("-"+ output + "-%n");
 		
 	}
 	public static void printTableLine(String tableLine) {
